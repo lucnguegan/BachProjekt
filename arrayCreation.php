3c5e6e1ba1f13c392c1122b45c952baf897fba5d
Luc NGUEGAN <lucpointcom@yahoo.fr>
2017-06-08 00:22:43 +0200

    Adding table, php code and css for the table

--- arrayCreation.php
+++ arrayCreation.php
@@ -1,5 +1,7 @@
 <?php
 
-echo "This is our array";
-
-?>
+$computers = array(
+    'Name', 'IP-Adresse', 'MAC-Adresse', 'Subnet Mask', 'OS');
+//echo '<pre>', print_r($computers), '</pre>';
+//echo "this is our php";
+print_r($computers);
