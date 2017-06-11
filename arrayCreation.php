8b532b4a8606dfc2d9729bf8529f4dd201cc78aa
Luc NGUEGAN <lucpointcom@yahoo.fr>
2017-06-11 22:19:46 +0200

    Ajout du formulaire de creation des données d'un ordinateur dans le fichier Formularhtml.html -

--- arrayCreation.php
+++ arrayCreation.php
@@ -1,39 +1,106 @@
-<?php
+<?php 
 
-$computers = array(
-    'Name', 'IP-Adresse', 'MAC-Adresse', 'Subnet Mask', 'OS');
-//echo '<pre>', print_r($computers), '</pre>';
-//echo "this is our php";
-print_r($computers);
+    $abt = filter_input(INPUT_POST, 'abteilung');
+    if($abt === 'Anmeldung')
+    {
+        echo 'Sie haben die Anmeldung gewählt';
+                
+    } else if($abt === 'Buchhaltung')
+    {
+        echo 'Sie haben die Buchhaltung ausgewälht';
+    } else if($abt === 'MRT')
+    {
+        echo 'Sie haben das MRT ausgewälht';
+    } else if($abt === 'CT')
+    {
+        echo 'Sie haben das CT ausgewälht';
+    } else if($abt === 'Röntgen')
+    {
+        echo 'Sie haben das Röntgen ausgewälht';
+    } 
 ?>
 
 <?php
-class computer
+class Computer
 {
-    private $abteilung;
-    private $hersteller;
-    private $ipadr;
-    private $macadr;
-    private $submask;
-    private $computers = array($abteilung, $hersteller, $ipadr, $macadr, $submask);
+    private $_abteilung;    //Departement for the computer
+    private $_hersteller;   //Manufacturer of the computer
+    private $_ipadr;        //IP-Adress
+    private $_macadr;       //MAC-Adress
+    private $_submask;      //Subnet Mask of the computer
+    private $_os;           //Operatingsystem of the computer
+    private $_remover;      //Remover of the computer on the list
     
-    public function __construct($abteilung, $hersteller, $ipadr, $macadr, $submask)
+    
+    //private $computers = array(abteilung, $hersteller, $ipadr, $macadr, $submask);
+    
+   public function __construct($abteilung, $hersteller, $ipadr, $macadr, $submask, $os) //When the departement is "Anmeldung
     {
         
-        this-$abteilung = $abteilung;
-        this-$hersteller = $hersteller;
-        this-$ipadr = $ipadr;
-        this-$macadr = $macadr;
-        this-$submask = $submask;
+        $this->setAbteilung($abteilung);
+        $this->setHersteller($hersteller);
+        $this->setIpadr($ipadr);
+        $this->setMacadr($macadr);
+        $this->setSubmask($submask);
+        $this->setOs($os);
+        
+        
+        
     }
     
-    for ($index = 0;
-    $index < count($array);
-    $index++) {
+    public function setAbteilung($abteilung)
+    {
+        $this->_abteilung = $abteilung;
+    }
     
-}
+    public function getAbteilung()
+    {
+        return $this->_abteilung;
+    }
+
+        public function setHersteller($hersteller)
+    {
+        $this->_hersteller = $hersteller;
+    }
+    
+    public function setIpadr($ipadr)
+    {
+        $this->_ipadr = $ipadr;
+    }
+    
+    public function setMacadr($macadr)
+    {
+        $this->_macadr = $macadr;
+    }
+    
+    public function setSubmask($submask)
+    {
+        $this->_submask = $submask;
+    }
+    
+    public function setOs($os)
+    {
+        $this->_os = $os;
+    }
+    
     
     
 }
 ?>
+<button type="button">Löschen</button>
+<?php  
+
+$rechner = new Computer('Anmeldung', 'IBM', '192.168.0.22', '11:22:33:44:55:66', '255.255.255.0', 'Archlinux');
+?>
+<br><br><br>
+
+<form action ="index.html" id="botbut"> 
+    <input id ="returnToStart" type="submit" value="Zurück zur Startseite" />
+</form>
+
+<form action ="Formularhtml.html" id="botbut"> 
+    <input id ="" type="submit" value="Zu formular" />
+</form>
+    
+
 
