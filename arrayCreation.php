0d4f7e70e96fa1b085215824b9ba58e9a8a002d9
Luc NGUEGAN <lucpointcom@yahoo.fr>
2017-06-12 12:39:32 +0200

    Ajout du fichier datenPruefen.php pour la vérification des données saisies - Ajout du fichier inserthtml.html pour l'ajout des données dans le tableau. - Suppresion du fichier datenPruefen.php. - Insertion de la selection du departement dans le fichier datenPruefen.

--- arrayCreation.php
+++ arrayCreation.php
@@ -1,5 +1,6 @@
 <?php 
 
+    
     $abt = filter_input(INPUT_POST, 'abteilung');
     if($abt === 'Anmeldung')
     {
@@ -19,7 +20,9 @@
         echo 'Sie haben das Röntgen ausgewälht';
     } 
 ?>
-
+<form method="post">
+    $_abt = $abt;
+</form>
 <?php
 class Computer
 {
@@ -44,7 +47,7 @@ class Computer
         $this->setSubmask($submask);
         $this->setOs($os);
         
-        
+       
         
     }
     
@@ -86,21 +89,9 @@ class Computer
     
     
 }
-?>
-<button type="button">Löschen</button>
-<?php  
-
 $rechner = new Computer('Anmeldung', 'IBM', '192.168.0.22', '11:22:33:44:55:66', '255.255.255.0', 'Archlinux');
 ?>
 <br><br><br>
 
-<form action ="index.html" id="botbut"> 
-    <input id ="returnToStart" type="submit" value="Zurück zur Startseite" />
-</form>
-
-<form action ="Formularhtml.html" id="botbut"> 
-    <input id ="" type="submit" value="Zu formular" />
-</form>
-    
 
 
