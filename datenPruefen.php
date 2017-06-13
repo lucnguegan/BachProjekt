e3abe7572b051c146a093ba508b93ff4b92d8485
Luc NGUEGAN <lucpointcom@yahoo.fr>
2017-06-13 13:56:35 +0200

    Réecriture compète du fichier datenPruefen.php

--- datenPruefen.php
+++ datenPruefen.php
@@ -1,1 +1,47 @@
 <?php
+echo "Prüfen Sie bitte die Daten, die Sie eingetragen haben: <br><br>";
+//Abteilung 
+$abt = filter_input(INPUT_POST, 'abt');
+echo "Abteilung: ".$abt."<br><br>";
+
+//Hersteller
+$her = filter_input(INPUT_POST, 'manufacturer');
+echo "Hersteller: ".$her."<br><br>";
+        
+//IP-Adresse
+$ip0 = filter_input(INPUT_POST, 'ipadr0');
+$ip1 = filter_input(INPUT_POST, 'ipadr1');
+$ip2 = filter_input(INPUT_POST, 'ipadr2');
+$ip3 = filter_input(INPUT_POST, 'ipadr3');
+$ip = $ip0.".".$ip1.".".$ip2.".".$ip3;
+echo "IP-Adresse: ".$ip."<br><br>";
+
+//MAC-Adresse
+$mac0 = filter_input(INPUT_POST, 'mac0');
+$mac1 = filter_input(INPUT_POST, 'mac1');
+$mac2 = filter_input(INPUT_POST, 'mac2');
+$mac3 = filter_input(INPUT_POST, 'mac3');
+$mac4 = filter_input(INPUT_POST, 'mac4');
+$mac5 = filter_input(INPUT_POST, 'mac5');
+$mac = $mac0.":".$mac1.":".$mac2.":".$mac3.":".$mac4.":".$mac5;
+echo "MAC-Adresse: ".$mac."<br><br>";
+
+//Subnet Mask
+$sub0 = filter_input(INPUT_POST, 'sub0');
+$sub1 = filter_input(INPUT_POST, 'sub1');
+$sub2 = filter_input(INPUT_POST, 'sub2');
+$sub3 = filter_input(INPUT_POST, 'sub3');
+$sub = $sub0.".".$sub1.".".$sub2.".".$sub3;
+echo "Subnet Mask: ".$sub."<br><br>";
+
+//Betriebssystem
+$os = filter_input(INPUT_POST, 'os');
+echo "Betriebssystem: ".$os."<br><br>";
+?>
+
+<form action ="Formularhtml.html">
+    <input type="submit" Value = "Zurück zum Formular">
+</form>
+<form action =" ">
+    <input type="submit" Value = "Weiter">
+</form>
